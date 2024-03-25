12062f14cfd7658c605d9cc03bdee863d361e577
xnnixa <jenni.j.parpala@gmail.com>
2024-03-25 12:48:49 +0200

    compiles, rotate added

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -6,8 +6,26 @@
 
 void rotate(std::string inputFile, std::string outputFile) {
 	PNG pic(inputFile);
-	// Your code here
-	pic.writeToFile(outputFile);
+	unsigned int width = pic.width();
+	unsigned int height = pic.height();
+
+	// create rotatedPic with the same dimensions as the original pic
+	PNG rotatedPic(width, height);
+
+	// go over pixels
+	for (unsigned int y = 0; y < height; y++) {
+		for(unsigned int x = 0; x < width; x++) {
+			// coordinates in the rotated image
+			unsigned int newX = width - x - 1;
+			unsigned int newY = height - y - 1;
+
+			RGBAPixel* originalPixel = pic(x, y);
+
+			*rotatedPic(newX, newY) = *originalPixel;
+		}
+	}
+
+	rotatedPic.writeToFile(outputFile);
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
