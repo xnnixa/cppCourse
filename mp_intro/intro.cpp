b7f4e30e01f5cac2d1e05190d0e2025718b3d49c
xnnixa <jenni.j.parpala@gmail.com>
2024-03-25 21:38:49 +0200

    mp_intro finished

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -30,6 +30,29 @@ void rotate(std::string inputFile, std::string outputFile) {
 
 PNG myArt(unsigned int width, unsigned int height) {
 	PNG png(width, height);
-	// Your code here
+
+    RGBAPixel color1(200, 60, 60);   // red
+    RGBAPixel color2(60, 180, 60);   // green
+    RGBAPixel color3(60, 60, 196);   // blue
+	RGBAPixel color4(255, 255, 255); // white
+	
+    for (unsigned int y = 0; y < height; y++) {
+        for (unsigned int x = 0; x < width; x++) {
+            // calculate the distance to the center of the image
+            double distance = sqrt(pow(x - width / 2.0, 2) + pow(y - height / 2.0, 2));
+
+            // color based on the distance
+            if (distance < width / 3.9) {
+                *png(x, y) = color1;
+            } else if (distance < 1 * width / 3.0) {
+                *png(x, y) = color2;
+			} else if (distance < 1.5 * width / 3.0) {
+				*png(x, y) = color3;
+            } else {
+                *png(x, y) = color4;
+            }
+        }
+    }
+
 	return png;
 }
